8175940f85f1d3408d5ec335d7160868a84dd4c7
nachosala89 <nachosala89@gmail.com>
2022-03-08 20:20:15 -0300

    Create home page

---
+++ app/javascript/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class HomePage extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>Hello</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default HomePage
