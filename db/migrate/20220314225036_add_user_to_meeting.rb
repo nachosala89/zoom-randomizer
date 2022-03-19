ccc2a2e7a75634ca791698ce978d4a5a16471247
nachosala89 <nachosala89@gmail.com>
2022-03-19 18:42:22 -0300

    Change tables in the database

--- db/migrate/20220314225036_add_user_to_meeting.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToMeeting < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :meetings, :user, optional:true, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_14_225036) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_14_214743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "meetings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_meetings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.boolean "selected"
     t.bigint "meeting_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["meeting_id"], name: "index_users_on_meeting_id"
   end
 
-  add_foreign_key "meetings", "users"
   add_foreign_key "users", "meetings"
 end
