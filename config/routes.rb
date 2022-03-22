83fc92dcdce7e5595dcc19ac988ebf8e799725cb
nachosala89 <nachosala89@gmail.com>
2022-03-22 19:41:14 -0300

    Redirect /meetings to static controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "static#index"
+  get '/meeting/:id', to: 'static#index'
   namespace :v1 do
-    resources :meetings
+    resources :meetings do
+      resources :users
+    end
   end
 end
