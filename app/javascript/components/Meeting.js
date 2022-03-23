30f82ade928098bee45b5080b3af2108d9966544
nachosala89 <nachosala89@gmail.com>
2022-03-23 20:55:19 -0300

    Display unselected participants

--- app/javascript/components/Meeting.js
+++ app/javascript/components/Meeting.js
@@ -5,21 +5,24 @@ import axios from 'axios';
 const Meeting = () => {
   const [users, setUsers] = useState([]);
   const { id } = useParams();
+  let selected = [];
 
   useEffect(() => {
     axios.get(`http://127.0.0.1:3000/v1/meetings/${id}/users`)
-      .then((response) => {setUsers(response.data)});
+      .then((response) => {
+        setUsers(response.data);
+        console.log(response.data);
+        selected = users.filter((user) => user.selected);
+      });
   }, []);
 
-  //Todo: implement selected and unselected in state
-  
   return (
     <>
       <h1>This is the meeting you've just created</h1>
-      {(users.length === 0)
+      {(users.filter((user) => !user.selected).length === 0)
         ? <p>No participants yet.</p>
         : <ul>
-          {users.map((user) => (
+          {users.filter((user) => !user.selected).map((user) => (
             <li key={user.id}>
               <span>{user.name}</span>
             </li>
